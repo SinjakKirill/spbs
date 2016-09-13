866308d6c6b95f4ac4ac965b049052b0be241e49
SinjakKirill <sinjak.kirill@gmail.com>
2016-09-13 14:27:06 +0300

    lab_3

--- Lab_1_2/app/src/main/java/com/example/sinyakkirill/lab_1_2/MainActivity.java
+++ Lab_1_2/app/src/main/java/com/example/sinyakkirill/lab_1_2/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.sinyakkirill.lab_1_2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        for (int i = 0; i < 4; i++) {
+
+        }
+        TextFunction1 textFunction1;
+        Log.d("Error", "Hello");
+        TextView newText = (TextView) findViewById(R.id.NewText);
+        newText.setText("goodbye!");
     }
 
     //TODO Hello!
---
+++ Lab_1_2/app/src/main/java/com/example/sinyakkirill/lab_1_2/TextFunction1.java
@@ -0,0 +1,11 @@
+package com.example.sinyakkirill.lab_1_2;
+
+/**
+ * Created by Sinyak Kirill on 09.09.2016.
+ */
+public class TextFunction1 {
+
+    public String getValue(){
+        return "Hello from First project!";
+    }
+}
