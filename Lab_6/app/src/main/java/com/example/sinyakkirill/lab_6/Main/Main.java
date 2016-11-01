b29f05dcea64064bccf87dfc3a36e04138fc0e64
SinjakKirill <sinjak.kirill@gmail.com>
2016-11-01 14:19:39 +0300

    lab_4_5 and lab_7

---
+++ Lab_6/app/src/main/java/com/example/sinyakkirill/lab_6/Main/Main.java
@@ -0,0 +1,11 @@
+package com.example.sinyakkirill.lab_6.Main;
+
+/**
+ * Created by Sinyak Kirill on 10.10.2016.
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello!");
+    }
+}
